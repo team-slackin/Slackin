e629cff34504bd5a91fcbd10be154e1c77571ebc
carter scadden <carter@scaddenfamily.com>
2019-05-10 15:01:43 -0600

    Input Component fully functional, added hooks to some Components with an explanation, created dropzone component

--- src/Components/Home/PrivateMessages/PrivateMessagesWindow.jsx
+++ src/Components/Home/PrivateMessages/PrivateMessagesWindow.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PrivateMessageConstructor from './PrivateMessagesConstructor';
 
 function PrivateMessagesWindow() {
+  const {privateMessagesArray, setPrivateMessageArray} = useState([
+    {
+      friend_messages_id: 0,
+      friends_id: 1,
+      user_id: 0,
+      date_posted: 0,
+      message: 'test message'
+    }//each one of these will be a row
+  ]);
+  //privateMessageArray will hold an array of objects from friends_messages in our db.
+  //try and only grab the first 25-50 at a time for performance?
+
   return (
     <div>{/* Displays the actual messeges from the user and their friend */}
-      <h1>Private messages window</h1>
+      {privateMessagesArray.map(message=>{
+        <span key={`MessageKey:${message.date_posted}`}>{message.message}</span>
+      })}
     </div>
   );
 };
--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Input() {
+function Input(props) {
+  const {placeholder, name, changeState} = props;//required props
+  const [input, setInput] = useState({});
+  // creates a variable input and a function setInput which we use like this.setState();
+
+  changeState(input);
+  //Our input will take a function that will always be called changeState(), and it will take our input value.
+  //SO changeState={COMPONENTFUNCTION} will be a function that changes a components hooks state
+  //we will also use our placeholder and name as well
+  //<Input changeState={COMPONENTFUNCTION} placeholder={'text value here'} />
+
+  //so where we use <Input name='tim' placeholder={text here} changeState={hooksChangeStateFunction} />
+  //the hookschangestate will be 
+  //  changer(val) {
+  //    setState(name: val);
+  //  };
+
   return (
-    <div>{/*  */}
-      <input></input>
+    <div>
+      <input name={name} placeholder={placeholder} onChange={(e)=>{setInput({[e.target.name]: e.target.value})}} />
     </div>
   );
 };
