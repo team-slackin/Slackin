1b2b4516cab1dc6b62aadfeab1aff878ee965a61
carter scadden <carter@scaddenfamily.com>
2019-05-18 11:14:43 -0600

    friends list displays and looks much better

--- src/Components/UserToolbar/UserToolbar.jsx
+++ src/Components/UserToolbar/UserToolbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import { connect } from "react-redux"
 import { grabChannels } from "./../../Ducks/channelReducer"
 import { setUserStatus } from "./../../Ducks/userReducer"
--- src/Ducks/channelReducer.js
+++ src/Ducks/channelReducer.js
@@ -10,10 +10,11 @@ const initialState = {
 const GRAB_CHANNELS = "GRAB_CHANNELS";
 const USER_SELECTED_CHANNEL = "USER_SELECTED_CHANNEL";
 const UPLOAD_CHANNEL_IMAGE_TO_DB = "UPLOAD_CHANNEL_IMAGE_TO_DB";
+const REMOVE_SELECTED_CHANNEL = 'REMOVE_SELECTED_CHANNEL';
 
 
 export const uploadChannelImageToDb = (type, url, channel_id) => ({type: UPLOAD_CHANNEL_IMAGE_TO_DB, payload: axios.post(`/api/database/amazon-url/${type}`, {url, channel_id}).then(res => res.data).catch(err=>console.log(err))});
-
+export const removeSelectedChannel = () => ({type: 'REMOVE_SELECTED_CHANNEL', payload: null});
 
 
 export const grabChannels = user_id => {
@@ -30,18 +31,28 @@ export default function reducer(state = initialState, action) {
   const { type, payload } = action
 
   switch (type) {
-    case GRAB_CHANNELS + "_PENDING":
-      return { ...state }
-    case GRAB_CHANNELS + "_FULFILLED":
+    case GRAB_CHANNELS + "_PENDING": {
+      return { ...state };
+    };
+
+    case GRAB_CHANNELS + "_FULFILLED": {
       return {
         ...state,
-        userChannels: payload.data,
-        currentChannel: payload.data[0]
-      }
+        userChannels: payload.data
+      };
+    };
+      
+    case REMOVE_SELECTED_CHANNEL + '_PENDING': {
+      return {...state};
+    };
 
+    case REMOVE_SELECTED_CHANNEL + '_FULFILLED': {
+      return {...state, currentChannel: payload};
+    };
 
-    case USER_SELECTED_CHANNEL:
-      return { ...state, currentChannel: payload }
+    case USER_SELECTED_CHANNEL: {
+      return { ...state, currentChannel: payload };
+    };
 
     case UPLOAD_CHANNEL_IMAGE_TO_DB + '_PENDING': {
       return {...state};
