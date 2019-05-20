72736e1dc7dad6e48337510dbac06059dd4245ae
Brendan Schiller <schiller.brendan@gmail.com>
2019-05-20 10:38:59 -0600

    background image and logo

--- src/App.js
+++ src/App.js
@@ -1,60 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import {HashRouter, Link} from 'react-router-dom'
-import routes from './routes'
-import axios from 'axios';
-import { connect } from 'react-redux'
-import { updateIsUserLoggedIn } from './Ducks/userReducer'
+import React, { useState, useEffect } from "react"
+import "./App.css"
+import { HashRouter, Link } from "react-router-dom"
+import routes from "./routes"
+import axios from "axios"
+import { connect } from "react-redux"
+import { updateIsUserLoggedIn } from "./Ducks/userReducer"
 
-import {MuiThemeProvider, createMuiTheme} from '@material-ui/core';
-import {orange} from '@material-ui/core/colors';
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+// import { orange } from "@material-ui/core/colors"
 
 const theme = createMuiTheme({
   palette: {
-    primary: orange,
-    secondary: orange
-  },
-  typography: {
-    useNextVariants: true,
-  },
-});
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: "#e96745"
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    }
+    // secondary: {
+    //   light: "#0066ff",
+    //   main: "#0044ff",
+    //   // dark: will be calculated from palette.secondary.main,
+    //   contrastText: "#ffcc00"
+    // }
+    // // error: will use the default color
+  }
+})
 
 function App(props) {
+  useEffect(() => {
+    axios
+      .get(`/retrievesession/`)
+      .then(res => {
+        if (res.data.user_id) {
+          props.updateIsUserLoggedIn(res.data)
+        }
+      })
+      .catch(err =>
+        console.log(`Something happened while checking for req.session ${err}`)
+      )
+  }, [])
 
-  useEffect(()=>{
-
-
-    axios.get(`/retrievesession/`)
-    .then((res)=>{
-      if(res.data.user_id){
-        props.updateIsUserLoggedIn(res.data);
-      };
-    })
-    .catch(err=>console.log(`Something happened while checking for req.session ${err}`));
-
-  }, []);
-  
   return (
     <MuiThemeProvider theme={theme}>
-    <HashRouter>
-      <div className="App">
-      <header className="App-header">
-        <span>
-          <Link to='/container' style={{color: 'white', marginLeft: '20px', textDecorationLine: 'none'}}>Slackin</Link>
-          </span>
-        </header>
-        <div className="main-content-flex-box">
-          {routes}
+      <HashRouter>
+        <div className="App">
+          <header className="App-header">
+            <span>
+              <Link
+                to="/container"
+                style={{
+                  color: "white",
+                  marginLeft: "20px",
+                  textDecorationLine: "none"
+                }}
+              >
+                Slackin
+              </Link>
+            </span>
+          </header>
+          <div className="main-content-flex-box">{routes}</div>
         </div>
-      </div>
-    </HashRouter>
+      </HashRouter>
     </MuiThemeProvider>
-  );
-};
+  )
+}
 
 const mapStateToProps = reduxState => ({
   userReducer: reduxState.userReducer,
   channelReducer: reduxState.channelReducer
 })
 
-export default connect(mapStateToProps, { updateIsUserLoggedIn })(App);
+export default connect(
+  mapStateToProps,
+  { updateIsUserLoggedIn }
+)(App)
