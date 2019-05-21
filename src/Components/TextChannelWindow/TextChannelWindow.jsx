c770c725a48dd66c2f4e3c03ba0b41b4e89e913f
Brendan Schiller <schiller.brendan@gmail.com>
2019-05-21 15:55:16 -0600

    status

--- src/Components/TextChannelWindow/TextChannelWindow.jsx
+++ src/Components/TextChannelWindow/TextChannelWindow.jsx
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { connect } from "react-redux";
-import { Input } from "@material-ui/core";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import { connect } from "react-redux"
+import { Input } from "@material-ui/core"
 
-import TextChannelMessegeScreen from "./TextChannelMessegeScreen";
+import TextChannelMessegeScreen from "./TextChannelMessegeScreen"
 
-import Chatkit from "@pusher/chatkit-client";
-import UserToolbar from "../UserToolbar/UserToolbar";
+import Chatkit from "@pusher/chatkit-client"
+import UserToolbar from "../UserToolbar/UserToolbar"
 // import UsersInChannel from "../UsersInChannel/UsersInChannel";
 
-import FriendsList from "./../FriendsList/FriendsList";
-import UsersInChannel from "../UsersInChannel/UsersInChannel";
+import FriendsList from "./../FriendsList/FriendsList"
+import UsersInChannel from "../UsersInChannel/UsersInChannel"
 
 function TextChannelWindow(props) {
-  const [inputMessage, setMessage] = useState("");
-  const [roomMessages, setRoomMessages] = useState([]);
-  const [currentUser, setCurrentUser] = useState({});
-  
+  const [inputMessage, setMessage] = useState("")
+  const [roomMessages, setRoomMessages] = useState([])
+  const [currentUser, setCurrentUser] = useState({})
+
   useEffect(() => {
-    setRoomMessages([]);
-  }, [props.subChannelReducer.currentSubChannelChatKitId]);
+    setRoomMessages([])
+  }, [props.subChannelReducer.currentSubChannelChatKitId])
 
   //FIX AUTH ERROR being double ran - look at parent
   useEffect(() => {
     if (props.subChannelReducer.currentSubChannelChatKitId) {
-      console.log('am i getting ran over and over')
+      console.log("am i getting ran over and over")
       const chatManager = new Chatkit.ChatManager({
         instanceLocator: "v1:us1:80870939-de37-40f2-aadc-dd3ee990b173",
         userId: `${props.userReducer.user.user_display_name}`,
@@ -32,68 +32,68 @@ function TextChannelWindow(props) {
           url:
             "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/80870939-de37-40f2-aadc-dd3ee990b173/token"
         })
-      });
+      })
 
       chatManager
         .connect()
         .then(currentUser => {
-          setCurrentUser({ currentUser });
+          setCurrentUser({ currentUser })
           return currentUser.subscribeToRoom({
             roomId: `${props.subChannelReducer.currentSubChannelChatKitId}`,
             messageLimit: 100,
             hooks: {
               onMessage: message => {
-                setRoomMessages(prevMessages => [...prevMessages, message]);
+                setRoomMessages(prevMessages => [...prevMessages, message])
               }
             }
-          });
+          })
         })
-        .catch(error => console.error("error", error));
+        .catch(error => console.error("error", error))
     }
-  }, [props.subChannelReducer.currentSubChannel]);
+  }, [props.subChannelReducer.currentSubChannel])
 
   const createMessage = e => {
-    const { value } = e.target;
-    setMessage(value);
-  };
+    const { value } = e.target
+    setMessage(value)
+  }
 
   const sendMessage = (text, e) => {
-    e.preventDefault();
+    e.preventDefault()
     currentUser.currentUser.sendMessage({
       text,
       roomId: `${props.subChannelReducer.currentSubChannelChatKitId}`
-    });
-  };
+    })
+  }
 
   return (
     <>
-      <header>Sub Channel name</header>
       <div className="text-channel-flex-box">
         <div className="main-text-window">
+          <header>Sub Channel name</header>
           {/* Each Individual Messege */}
           <div className="main-screen">
             {roomMessages.map((message, index) => {
               return (
                 <TextChannelMessegeScreen key={index} roomMessage={message} />
-              );
+              )
             })}
           </div>
           <div className="main-text-input">
             <form>
               <Input
-                placeholder={'Send message here.'}
+                placeholder={"Send message here."}
                 onChange={e => {
-                  createMessage(e);
+                  createMessage(e)
                 }}
                 value={inputMessage}
                 fullWidth
               />
               <button
                 onClick={e => {
-                  sendMessage(inputMessage, e);
+                  sendMessage(inputMessage, e)
                 }}
                 style={{
-                  display: 'none'
+                  display: "none"
                 }}
               >
                 Temp Submit
@@ -103,11 +103,11 @@ function TextChannelWindow(props) {
         </div>
 
         <aside>
-                <UsersInChannel />
+          <UsersInChannel />
         </aside>
       </div>
     </>
-  );
+  )
 }
 
 const mapStateToProps = reduxState => {
@@ -115,10 +115,10 @@ const mapStateToProps = reduxState => {
     userReducer: reduxState.userReducer,
     subChannelReducer: reduxState.subChannelReducer,
     textChannelReducer: reduxState.textChannelReducer
-  };
-};
+  }
+}
 
 export default connect(
   mapStateToProps,
   {}
-)(TextChannelWindow);
+)(TextChannelWindow)
