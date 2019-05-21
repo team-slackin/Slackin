d8432b2c48019442b0d8df4a64d1fd6a53bb59d3
Brendan Schiller <schiller.brendan@gmail.com>
2019-05-20 21:05:05 -0600

    box shadow

--- src/Components/MainChannelNav/MainChannelNav.jsx
+++ src/Components/MainChannelNav/MainChannelNav.jsx
@@ -1,75 +1,83 @@
-import React, {useEffect, useState} from "react";
-import { connect } from "react-redux";
-import {grabChannels, removeSelectedChannel} from "./../../Ducks/channelReducer";
-import {Link} from 'react-router-dom';
-import Icon from '@material-ui/core/Icon';
+import React, { useEffect, useState } from "react"
+import { connect } from "react-redux"
+import {
+  grabChannels,
+  removeSelectedChannel
+} from "./../../Ducks/channelReducer"
+import { Link } from "react-router-dom"
+import Icon from "@material-ui/core/Icon"
 
 import MainChannelConstructor from "./MainChannelConstructor"
 
-import './MainChannelNav.scss';
+import "./MainChannelNav.scss"
 
 function MainChannelNav(props) {
-  const {user} = props.userReducer;
-  const [borderRadius, setBorderRadius] = useState('25');
+  const { user } = props.userReducer
+  const [borderRadius, setBorderRadius] = useState("25")
 
   useEffect(() => {
-    props.grabChannels(props.userReducer.user.user_id);
-  }, []);
-  
-  useEffect(()=>{
+    props.grabChannels(props.userReducer.user.user_id)
+  }, [])
+
+  useEffect(() => {
     console.log(`from MainChannelNav`, props.channelReducer)
     if (props.channelReducer.currentChannel) {
-      setBorderRadius('50');
+      setBorderRadius("50")
     } else {
-      setBorderRadius('25');
+      setBorderRadius("25")
     }
-  },[props.channelReducer.currentChannel])
+  }, [props.channelReducer.currentChannel])
 
   return (
     <>
       <div className="main-channel-nav">
-
         <div className="main-channel-nav-seperator">
           <Link to="/container">
-          <img 
-          src={user.user_image} 
-          className="main-channel-img"
-          onClick={()=>{
-            props.removeSelectedChannel();
-          }} 
-          style={{
-            borderRadius: `${borderRadius}%`,
-            width: '80px',
-            height: '80px'
-          }}
-          />
+            <img
+              src={user.user_image}
+              className="main-channel-img"
+              onClick={() => {
+                props.removeSelectedChannel()
+              }}
+              style={{
+                borderRadius: `${borderRadius}%`,
+                width: "80px",
+                height: "80px"
+              }}
+            />
           </Link>
         </div>
 
-        {props.channelReducer.userChannels[0] ?
-        props.channelReducer.userChannels.map((channel, i) => (
-        <MainChannelConstructor 
-          key={i} 
-          channel={channel} 
-          />
-        )) : (<></>)}
+        {props.channelReducer.userChannels[0] ? (
+          props.channelReducer.userChannels.map((channel, i) => (
+            <MainChannelConstructor key={i} channel={channel} />
+          ))
+        ) : (
+          <></>
+        )}
         <div className="plus-sign-div">
-          <Icon style={{
-            fontSize: '3em',
-            color: 'var(--main-color)',
-            textShadow: 'var(--text-icon-shadow)'
-          }}>add</Icon>
+          <Icon
+            style={{
+              fontSize: "3em",
+              color: "var(--main-color)",
+              textShadow: "var(--text-icon-shadow-1)"
+            }}
+          >
+            add
+          </Icon>
         </div>
-
       </div>
     </>
   )
 }
 
 const mapStateToProps = reduxState => ({
-    channelReducer: reduxState.channelReducer,
-    userReducer: reduxState.userReducer,
-    mainChannelReducer: reduxState.mainChannelReducer
-});
+  channelReducer: reduxState.channelReducer,
+  userReducer: reduxState.userReducer,
+  mainChannelReducer: reduxState.mainChannelReducer
+})
 
-export default connect(mapStateToProps, {grabChannels, removeSelectedChannel})(MainChannelNav);
+export default connect(
+  mapStateToProps,
+  { grabChannels, removeSelectedChannel }
+)(MainChannelNav)
--- src/Components/UserToolbar/UserToolbar.jsx
+++ src/Components/UserToolbar/UserToolbar.jsx
@@ -1,140 +1,155 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react"
 import { connect } from "react-redux"
 import { grabChannels } from "./../../Ducks/channelReducer"
 import { setUserStatus } from "./../../Ducks/userReducer"
-import {Link} from 'react-router-dom'
-import Icon from '@material-ui/core/Icon';
-import './UserToolbar.scss';
-import {Button} from '@material-ui/core';
+import { Link } from "react-router-dom"
+import Icon from "@material-ui/core/Icon"
+import "./UserToolbar.scss"
+import { Button } from "@material-ui/core"
 
 function UserToolbar(props) {
-    const [editStatusFlag, setEditStatusFlag] = useState('none');
-    const [fade, setFade] = useState('fadeIn');
-    const [buttonDisplay, setButtonDisplay] = useState('inline-flex');
-    const [visable, setVisable] = useState('hidden');
-    
+  const [editStatusFlag, setEditStatusFlag] = useState("none")
+  const [fade, setFade] = useState("fadeIn")
+  const [buttonDisplay, setButtonDisplay] = useState("inline-flex")
+  const [visable, setVisable] = useState("hidden")
 
-    const [currentUserStatusColor, setCurrentUserStatusColor] = useState('#689f38');
+  const [currentUserStatusColor, setCurrentUserStatusColor] = useState(
+    "#689f38"
+  )
 
-    let toggleStatusEdit = ()=>{
-      if (editStatusFlag === 'none') {
-        setEditStatusFlag('flex');
-        setFade('fadeIn');
-        setButtonDisplay('inline-flex');
-        setVisable('unset');
-      } else {
-        setFade('fadeOut');
-        setEditStatusFlag('none');
-        setButtonDisplay('none');
-        setVisable('hidden');
-      };
-    };
+  let toggleStatusEdit = () => {
+    if (editStatusFlag === "none") {
+      setEditStatusFlag("flex")
+      setFade("fadeIn")
+      setButtonDisplay("inline-flex")
+      setVisable("unset")
+    } else {
+      setFade("fadeOut")
+      setEditStatusFlag("none")
+      setButtonDisplay("none")
+      setVisable("hidden")
+    }
+  }
 
-    useEffect(()=> {
-      setVisable('hidden');
-      setEditStatusFlag('none');
-      setFade('none');
-      setButtonDisplay('none');
-    }, []);
+  useEffect(() => {
+    setVisable("hidden")
+    setEditStatusFlag("none")
+    setFade("none")
+    setButtonDisplay("none")
+  }, [])
 
   return (
     <aside className="user-tool-bar">
-
-        <div 
-          className="edit-status-container"
+      <div
+        className="edit-status-container"
+        style={{
+          visibility: visable,
+          display: setEditStatusFlag,
+          animationName: fade
+        }}
+      >
+        <Button
+          onClick={() => {
+            props.setUserStatus("online")
+            setCurrentUserStatusColor("#689f38")
+          }}
           style={{
-            visibility: visable,
-            display: setEditStatusFlag,
-            animationName: fade
+            display: buttonDisplay,
+            animationName: fade,
+            animationDuration: "1s"
           }}
         >
+          Online
+        </Button>
 
-          <Button 
-            onClick={()=>{
-              props.setUserStatus('online');
-              setCurrentUserStatusColor('#689f38');
-            }}
-              style={{
-                display: buttonDisplay,
-                animationName: fade,
-                animationDuration: '1s'
-              }}
-            >Online</Button>
-          
-          <Button 
-            onClick={()=>{
-              props.setUserStatus('idle');
-              setCurrentUserStatusColor('white');
-            }}
-              style={{
-                display: buttonDisplay,
-                animationName: fade,
-                animationDuration: '1s'
-              }}
-            >Idle</Button>
-          
-          <Button 
-            onClick={()=>{
-              props.setUserStatus('do not disturb');
-              setCurrentUserStatusColor('red');
-            }}
-              style={{
-                display: buttonDisplay,
-                animationName: fade,
-                animationDuration: '1s'
-              }}
-            >Do Not Disturb</Button>
-          
-          <Button 
-            onClick={()=>{
-              props.setUserStatus('invisible');
-              setCurrentUserStatusColor('gray');
-            }}
-              style={{
-                display: buttonDisplay,
-                animationName: fade,
-                animationDuration: '1s'
-              }}
-            >Invisible</Button>
-        </
-        div>
-      
+        <Button
+          onClick={() => {
+            props.setUserStatus("idle")
+            setCurrentUserStatusColor("white")
+          }}
+          style={{
+            display: buttonDisplay,
+            animationName: fade,
+            animationDuration: "1s"
+          }}
+        >
+          Idle
+        </Button>
 
-        <div className="user-tool-bar-information">
-          <div className='image-container'>
-            <img 
-              src={props.userReducer.user.user_image} 
-              className="user-tool-bar-image"
-              onClick={()=>{toggleStatusEdit()}} />
-          </div>
+        <Button
+          onClick={() => {
+            props.setUserStatus("do not disturb")
+            setCurrentUserStatusColor("red")
+          }}
+          style={{
+            display: buttonDisplay,
+            animationName: fade,
+            animationDuration: "1s"
+          }}
+        >
+          Do Not Disturb
+        </Button>
 
-          <div className="user-tool-bar-username">{props.userReducer.user.user_display_name}</div>
-          <div className="user-tool-bar-status" style={{backgroundColor: `${currentUserStatusColor}`}}></div>
-        
-          <div className="user-tool-bar-cog">
-          <Link to='/container/account'>
-            <Icon style={{
-              fontSize: '2em',
-              color: 'var(--main-color)',
-              textShadow: 'var(--text-icon-shadow)'
-            }}>settings</Icon>
-          </Link>
-          </div>
+        <Button
+          onClick={() => {
+            props.setUserStatus("invisible")
+            setCurrentUserStatusColor("gray")
+          }}
+          style={{
+            display: buttonDisplay,
+            animationName: fade,
+            animationDuration: "1s"
+          }}
+        >
+          Invisible
+        </Button>
+      </div>
+
+      <div className="user-tool-bar-information">
+        <div className="image-container">
+          <img
+            src={props.userReducer.user.user_image}
+            className="user-tool-bar-image"
+            onClick={() => {
+              toggleStatusEdit()
+            }}
+          />
         </div>
 
+        <div className="user-tool-bar-username">
+          {props.userReducer.user.user_display_name}
+        </div>
+        <div
+          className="user-tool-bar-status"
+          style={{ backgroundColor: `${currentUserStatusColor}` }}
+        />
+
+        <div className="user-tool-bar-cog">
+          <Link to="/container/account">
+            <Icon
+              style={{
+                fontSize: "2em",
+                color: "var(--main-color)",
+                textShadow: "var(--text-icon-shadow)"
+              }}
+            >
+              settings
+            </Icon>
+          </Link>
+        </div>
+      </div>
     </aside>
   )
 }
 
 const mapStateToProps = reduxState => {
-    return {
-      channelReducer: reduxState.channelReducer,
-      userReducer: reduxState.userReducer
-    }
+  return {
+    channelReducer: reduxState.channelReducer,
+    userReducer: reduxState.userReducer
   }
-  
-  export default connect(
-    mapStateToProps,
-    { grabChannels, setUserStatus }
-  )(UserToolbar)
-  
+}
+
+export default connect(
+  mapStateToProps,
+  { grabChannels, setUserStatus }
+)(UserToolbar)
