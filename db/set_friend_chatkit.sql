15ba98a2e566d0815a4ccee9b4e7569fe8a19e7d
Patrick McNamee <pjmcnamee@comcast.net>
2019-05-22 15:02:17 -0600

    friend chat works and can switch between subchannel chat and friend chat at will

--- db/set_friend_chatkit.sql
+++ db/set_friend_chatkit.sql
@@ -27,3 +27,23 @@ UPDATE friends
   SET chatkit_private = $3
   WHERE friends.friend_id = $5
     AND friends.user_id = $4;
+
+  UPDATE friends 
+  SET room_created = true
+  WHERE friends.friend_id = $4 
+    AND friends.user_id = $5;
+
+    UPDATE friends 
+  SET room_created = true
+  WHERE friends.friend_id = $5 
+    AND friends.user_id = $4;
+
+     UPDATE friends 
+  SET friends_chatkit_id = $6
+  WHERE friends.friend_id = $5 
+    AND friends.user_id = $4;
+
+     UPDATE friends 
+  SET friends_chatkit_id = $7
+  WHERE friends.friend_id = $4
+    AND friends.user_id = $5;
--- src/Components/TextChannelWindow/TextChannelWindow.jsx
+++ src/Components/TextChannelWindow/TextChannelWindow.jsx
@@ -1,31 +1,52 @@
-import React, { useState, useEffect } from "react"
-import axios from "axios"
-import { connect } from "react-redux"
-import { Input } from "@material-ui/core"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { connect } from "react-redux";
+import { Input } from "@material-ui/core";
 
 import TextChannelMessegeScreen from "./TextChannelMessegeScreen";
 import _ from "lodash";
 
-
-import Chatkit from "@pusher/chatkit-client"
-import UserToolbar from "../UserToolbar/UserToolbar"
+import Chatkit from "@pusher/chatkit-client";
+import UserToolbar from "../UserToolbar/UserToolbar";
 // import UsersInChannel from "../UsersInChannel/UsersInChannel";
 
-import FriendsList from "./../FriendsList/FriendsList"
-import UsersInChannel from "../UsersInChannel/UsersInChannel"
+import FriendsList from "./../FriendsList/FriendsList";
+import UsersInChannel from "../UsersInChannel/UsersInChannel";
 
-import { setReduxMessage } from './../../Ducks/textChannelReducer'
+import {
+  setReduxMessage,
+  resetReduxMessage
+} from "./../../Ducks/textChannelReducer";
+
 
 function TextChannelWindow(props) {
   const [inputMessage, setMessage] = useState("");
   const [roomMessages, setRoomMessages] = useState([]);
   const [currentUser, setCurrentUser] = useState({});
+  
+  //roomID holder - switched with useEffect that looks for correct channels
+  const [roomID, setRoomId] = useState('')
 
   //FIX AUTH ERROR being double ran - look at parent
 
-  useEffect(()=>{
 
-      console.log("am i getting ran over and over")
+  useEffect(() => {
+    if(props.subChannelReducer.currentSubChannelChatKitId){
+      setRoomId(`${props.subChannelReducer.currentSubChannelChatKitId}`)
+    }else if (props.friendReducer.currentFriend.chatkit_id){
+      setRoomId(`${props.friendReducer.currentFriend.chatkit_id}`)
+    } else {
+      setRoomId('null')
+    }
+  },[props.subChannelReducer.currentSubChannelChatKitId])
+  
+  useEffect(() => {
+    props.resetReduxMessage()
+  },[roomID])
+
+  useEffect(() => {
+    console.log("am i getting ran over and over");
+    if(roomID !== 'null'){
       const chatManager = new Chatkit.ChatManager({
         instanceLocator: "v1:us1:80870939-de37-40f2-aadc-dd3ee990b173",
         userId: `${props.userReducer.user.user_display_name}`,
@@ -33,48 +54,45 @@ function TextChannelWindow(props) {
           url:
             "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/80870939-de37-40f2-aadc-dd3ee990b173/token"
         })
-      })
-
+      });
+  
       // _.debounce(setRoomMessages(prevMessages => [...prevMessages, message]), 250)
-
+  
       chatManager
         .connect()
-
+  
         .then(async currentUser => {
           setCurrentUser({ currentUser });
           const data = await currentUser.subscribeToRoom({
-            roomId: `${props.subChannelReducer.currentSubChannelChatKitId}`,
+            roomId: roomID,
             messageLimit: 100,
             hooks: {
               onMessage: message => {
-
-                props.setReduxMessage(message)
-
+                props.setReduxMessage(message);
               }
             }
-          })
+          });
         })
-        .catch(error => console.error("error", error))
-  }, [props.subChannelReducer.currentSubChannel])
+        .catch(error => console.error("error", error));
+    }
+  }, [roomID]);
 
-
-  
   const createMessage = e => {
-    const { value } = e.target
-    setMessage(value)
-  }
-
-  const sendMessage = (text, e) => {
-    e.preventDefault()
-    currentUser.currentUser.sendMessage({
-      text,
-      roomId: `${props.subChannelReducer.currentSubChannelChatKitId}`
-    });
-    setMessage('')
+    const { value } = e.target;
+    setMessage(value);
   };
 
-
-  console.log('TEXT CHANNEL REDUCER 1111111111',props)
+  const sendMessage = (text, e) => {
+    e.preventDefault();
+    currentUser.currentUser.sendMessage({
+      text,
+      roomId: roomID
+    });
+    setMessage("");
+  };
+  console.log('SUBCHANNEL CHATKIT ID', props.subChannelReducer.currentSubChannelChatKitId)
+  console.log("CHECK FOR FRIEND CHANNEL ID", props.friendReducer.currentFriend.chatkit_id);
+  console.log('ROOMID FOR FRIENDS CHAT', roomID)
   return (
     <>
       <div className="text-channel-flex-box">
@@ -86,7 +104,7 @@ function TextChannelWindow(props) {
               {props.textChannelReducer.messages.map((message, index) => {
                 return (
                   <TextChannelMessegeScreen key={index} roomMessage={message} />
-                )
+                );
               })}
             </article>
           </div>
@@ -95,14 +113,14 @@ function TextChannelWindow(props) {
               <Input
                 placeholder={"Send message here."}
                 onChange={e => {
-                  createMessage(e)
+                  createMessage(e);
                 }}
                 value={inputMessage}
                 fullWidth
               />
               <button
                 onClick={e => {
-                  sendMessage(inputMessage, e)
+                  sendMessage(inputMessage, e);
                 }}
                 style={{
                   display: "none"
@@ -119,19 +137,19 @@ function TextChannelWindow(props) {
         </aside>
       </div>
     </>
-  )
+  );
 }
 
 const mapStateToProps = reduxState => {
   return {
     userReducer: reduxState.userReducer,
     subChannelReducer: reduxState.subChannelReducer,
-    textChannelReducer: reduxState.textChannelReducer
-  }
-}
+    textChannelReducer: reduxState.textChannelReducer,
+    friendReducer: reduxState.friendReducer
+  };
+};
 
 export default connect(
   mapStateToProps,
-  {setReduxMessage}
+  { setReduxMessage, resetReduxMessage }
 )(TextChannelWindow);
-
