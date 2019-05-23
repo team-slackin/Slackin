06115082e18232a49bc38b1efc5b727a630de97d
Brendan Schiller <schiller.brendan@gmail.com>
2019-05-23 09:13:46 -0600

    status color

--- src/Components/FriendsConstructor/FriendsConstructor.jsx
+++ src/Components/FriendsConstructor/FriendsConstructor.jsx
@@ -1,75 +1,82 @@
-import React, {useState, useEffect} from 'react';
-import {connect} from 'react-redux';
-import Chatkit from "@pusher/chatkit-client";
-import Axios from 'axios';
+import React, { useState, useEffect } from "react"
+import { connect } from "react-redux"
+import Chatkit from "@pusher/chatkit-client"
+import Axios from "axios"
 
-import {makeCurrentFriend, grabFriends} from '../../Ducks/friendReducer'
-const FriendsConstructor = (props) => {
-  const {user_status, room_created, user_id, user_display_name} = props.friend;
-  const [currentUserStatusColor, setCurrentUserStatusColor] = useState('#689f38');
-  
-  useEffect(()=>{
-    switch(user_status) {
-      case 'online': {
-        setCurrentUserStatusColor('#689f38');
-        break;
-      };
-      case 'idle': {
-        setCurrentUserStatusColor('yellow');
-        break;
-      };
-      case 'do not disturb': {
-        setCurrentUserStatusColor('red');
-        break;
-      };
-      case 'offline': {
-        setCurrentUserStatusColor('gray');
-        break;
-      };
+import { makeCurrentFriend, grabFriends } from "../../Ducks/friendReducer"
+const FriendsConstructor = props => {
+  const { user_status, room_created, user_id, user_display_name } = props.friend
+  const [currentUserStatusColor, setCurrentUserStatusColor] = useState(
+    "#43b581"
+  )
+
+  useEffect(() => {
+    switch (user_status) {
+      case "online": {
+        setCurrentUserStatusColor("#43b581")
+        break
+      }
+      case "idle": {
+        setCurrentUserStatusColor("#faa61a")
+        break
+      }
+      case "do not disturb": {
+        setCurrentUserStatusColor("#f04747")
+        break
+      }
+      case "offline": {
+        setCurrentUserStatusColor("#747f8d")
+        break
+      }
       default: {
-        setCurrentUserStatusColor('gray');
-        break;
-      };
-    };
-  }, []);
+        setCurrentUserStatusColor("gray")
+        break
+      }
+    }
+  }, [])
 
-  const friendRoomMakeOrCreate = () => {//temp token and instance id
-
+  const friendRoomMakeOrCreate = () => {
+    //temp token and instance id
 
     /*## Create room if the room hasnt been created##*/
     if (!room_created) {
-      Axios.post("/chatkit/createroom/friends", {user_display_name, user_id})
-        .catch(err=>console.log(err));
+      Axios.post("/chatkit/createroom/friends", {
+        user_display_name,
+        user_id
+      }).catch(err => console.log(err))
     } else {
-
-    };
+    }
 
     /*## Continue ##*/
-    Axios.put('/api/friend-room-created', {user_id})
-    props.makeCurrentFriend(props.friend);
-    props.grabFriends();
+    Axios.put("/api/friend-room-created", { user_id })
+    props.makeCurrentFriend(props.friend)
+    props.grabFriends()
     //go to friends chat window .jsx
-  };
+  }
 
   return (
-    <div 
+    <div
       className="friends-list-flex-box"
-      onClick={()=>{friendRoomMakeOrCreate()}}
+      onClick={() => {
+        friendRoomMakeOrCreate()
+      }}
     >
-      <img 
+      <img
         className="friends-list-image"
-        src={props.friend.user_image} 
-        alt={props.friend.user_display_name} 
+        src={props.friend.user_image}
+        alt={props.friend.user_display_name}
       />
       <span>{`${props.friend.user_display_name}`}</span>
       <div className="friend-status">
-        <div style={{backgroundColor: `${currentUserStatusColor}`}}></div>
+        <div style={{ backgroundColor: `${currentUserStatusColor}` }} />
       </div>
     </div>
-  );
-};
+  )
+}
 
 const mapStateToProps = reduxState => reduxState.userReducer
 
-export default connect(mapStateToProps, {makeCurrentFriend, grabFriends}) (FriendsConstructor);
-
+export default connect(
+  mapStateToProps,
+  { makeCurrentFriend, grabFriends }
+)(FriendsConstructor)
