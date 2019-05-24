b05b0a0aaa7fb27372db0943513458d9bd53a268
carter scadden <carter@scaddenfamily.com>
2019-05-24 08:52:08 -0600

    channels overflow is fixed

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -13,7 +13,7 @@ import signIn from "./../../Assets/sign-in.png"
 const styles = theme => {
   return {
     root: {
-      background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      background: 'rgba(204, 204, 204, 0.36)',
       border: 0,
       borderRadius: 3,
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
